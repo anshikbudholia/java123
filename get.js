a18e087d0fa806862dbe548efa49e9e62d2ff867
anshikbudholia <budholiaanshika@gmail.com>
2024-02-08 21:45:35 +0530

    comm

---
+++ get.js
@@ -0,0 +1,8 @@
+document.write("hello")
+
+var data=document.getElementById("a").innerText;
+data=document.getElementById("a").innerHtml;
+data=document.getElementById("a").getAttribute("style");
+data=document.getElementById("a").attribute[0];
+
+console.log(data);
---
+++ set.js
@@ -0,0 +1,4 @@
+document.getElementById("d").innerHTML = "<center><h1>PNINFOSYs GWALIOR</h1></center>";
+document.getElementById("d").setAttribute("style","border:10px dotted red");
+//document.getElementById("d").removeAttribute("style");
+//document.getElementById("e").innerHTML = "<h1>PNINFOSYS</h1>";
